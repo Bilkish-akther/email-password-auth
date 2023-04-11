2bee26961317de2f7bb53db4c04423f820d9dbc9
Programming Hero <programming.hero1@gmail.com>
2023-04-10 22:58:55 -0500

    explore react bootstrap and bootstrap form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,8 @@ const Header = () => {
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
+            <Link to="/register-rbs">Register RBS</Link>
+            <Link to="/register-bs">Register BS</Link>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -10,6 +10,8 @@ import Main from './components/layout/Main';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import RegisterRBS from './components/RegisterRBS/RegisterRBS';
+import RegisterBS from './components/RegisterBS/RegisterBS';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,14 @@ const router = createBrowserRouter([
       {
         path: '/register',
         element: <Register></Register>
+      }, 
+      {
+        path: '/register-rbs',
+        element: <RegisterRBS></RegisterRBS>
+      },
+      {
+        path: '/register-bs',
+        element: <RegisterBS></RegisterBS>
       }
     ]
   },

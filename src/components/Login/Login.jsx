121757dd2a916f2809ef9d4d46176ec2855f2329
Programming Hero <programming.hero1@gmail.com>
2023-04-11 18:18:25 -0500

    recap login validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+        // validation
+        setError('');
+        setSuccess('');
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setError('Please add at least two uppercase.');
+            return
+        }
+        else if(!/(?=.*[!@#$&*])/.test(password)){
+            setError('Please add a special character.');
+            return
+        }
+        else if(password.length < 6) {
+            setError('Password must be 6 characters long');
+            return
+        }
+
+        
+
+    }
+
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='w-25 mx-auto'>
+            <h2>Please Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-group mb-3">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" name='email' className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required />
+                    
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' className="form-control" id="password" placeholder="Password" required />
+                </div>
+                <div className="form-check mb-3">
+                    <input type="checkbox" className="form-check-input" id="rememberMe" />
+                    <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
